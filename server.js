7f464566f1e23e2ba14a777b76703a61eb70af82
Rhone Lachner <fernandclay@gmail.com>
2020-11-09 15:59:03 -0800

    updated url path location

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 
 
-app.get('/', async(req, res) => {
+app.get('/location:search', async(req, res) => {
   try {
     const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_KEY}&q=${req.query.search}&format=json`;
   
@@ -25,8 +25,6 @@ app.get('/', async(req, res) => {
   
     const newResponse = mungeLocation(response.body);
 
-    // STRICTLY TYPED language
-    // TYPESCRIPT is a SUPERSET of JS
   
     res.json(newResponse);
   } catch(e) {
