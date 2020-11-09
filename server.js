ef189fedaadfe92a7020124e7176840913a8333b
Rhone Lachner <fernandclay@gmail.com>
2020-11-09 15:51:44 -0800

    setup data, utils, and server for location api

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
+import { mungeLocation } from './utils.js';
+
 require('dotenv').config();
 require('./lib/client').connect();
 
+
+
+const express = require('express');
+const cors = require('cors');
+const request = require('superagent');
 const app = require('./lib/app');
+const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 7890;
+app.use(cors());
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Started on ${PORT}`);
+
+
+
+app.get('/', async(req, res) => {
+  try {
+    const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_KEY}&q=${req.query.search}&format=json`;
+  
+    const response = await request.get(URL);
+  
+    const newResponse = mungeLocation(response.body);
+
+    // STRICTLY TYPED language
+    // TYPESCRIPT is a SUPERSET of JS
+  
+    res.json(newResponse);
+  } catch(e) {
+    res.json({ error: e.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
