3766a7bf23b975aacd696c68be370fe152f460dc
Rhone Lachner <fernandclay@gmail.com>
2020-11-09 16:49:48 -0800

    updated pathing step 2

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-import { mungeLocation } from './utils.js';
-
 require('dotenv').config();
 require('./lib/client').connect();
 
 
-
-const express = require('express');
-const cors = require('cors');
-const request = require('superagent');
 const app = require('./lib/app');
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+
 
 
 
