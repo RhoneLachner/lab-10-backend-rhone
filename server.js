31b42c0b99f2f21f21be83e9d2a2d83c930300da
Rhone Lachner <fernandclay@gmail.com>
2020-11-09 16:42:15 -0800

    moved get to app.js

--- server.js
+++ server.js
@@ -16,22 +16,6 @@ app.use(cors());
 
 
 
-
-app.get('/location', async(req, res) => {
-  try {
-    const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_KEY}&q=${req.query.search}&format=json`;
-  
-    const response = await request.get(URL);
-  
-    const newResponse = mungeLocation(response.body);
-
-  
-    res.json(newResponse);
-  } catch(e) {
-    res.json({ error: e.message });
-  }
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
