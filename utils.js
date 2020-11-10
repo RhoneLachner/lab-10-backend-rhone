059ba9fb2150c03db64d1a26df7c86e5a434c268
Rhone Lachner <fernandclay@gmail.com>
2020-11-09 18:23:38 -0800

    added weather

--- utils.js
+++ utils.js
@@ -7,7 +7,14 @@ function mungeLocation(location) {
   };
 }
 
+function mungeWeather(weather) {
+  return weather.data.map(item => {
+    return {        
+      forecast: item.weather.description,
+      time: item.datetime       
+    };
+  }).slice(0, 8);
+}
+  
 
-module.exports = {
-  mungeLocation
-};
+module.exports = { mungeLocation, mungeWeather };
